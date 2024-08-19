4719a81f01434e5e3ed7f6ec3f996b26b286590f
minhee231 <goominhee123@gmail.com>
2024-08-19 20:10:16 +0900

    vuetify 적용

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,56 @@
 <template>
-  <div id="app">
-    <h1>Vue.js To-Do App</h1>
-    <ToDoList />
-  </div>
+  <v-app>
+    <v-container>
+      <v-row>
+        <v-col>
+          <h1 class="text-center">Vue.js To-Do App</h1>
+          <AddTodo @add-todo="addTodo" />
+          <ToDoList :todos="todos" @remove-todo="removeTodo" @save-todos="saveTodos" />
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-app>
 </template>
 
 <script>
+import AddTodo from './components/AddTodo.vue';
 import ToDoList from './components/ToDoList.vue';
 
 export default {
   name: 'App',
   components: {
+    AddTodo,
     ToDoList
+  },
+  data() {
+    return {
+      todos: []
+    };
+  },
+  mounted() {
+    const savedTodos = localStorage.getItem('todos');
+    if (savedTodos) {
+      this.todos = JSON.parse(savedTodos);
+    }
+  },
+  methods: {
+    addTodo(newTodo) {
+      this.todos.push({ text: newTodo, completed: false });
+      this.saveTodos();
+    },
+    removeTodo(index) {
+      this.todos.splice(index, 1);
+      this.saveTodos();
+    },
+    saveTodos() {
+      localStorage.setItem('todos', JSON.stringify(this.todos));
+    }
   }
 };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+.text-center {
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
