5d8238b2a1dbbae855eec68c48d0e051b264e2cd
minhee231 <goominhee123@gmail.com>
2024-08-20 17:48:57 +0900

    router

---
+++ src/components/ToDoDetail.vue
@@ -0,0 +1,22 @@
+<template>
+    <v-container>
+      <h2>To-Do Detail</h2>
+      <v-card>
+        <v-card-text>
+          <p>ID: {{ id }}</p>
+        </v-card-text>
+      </v-card>
+      <v-btn @click="$router.push('/')">Back</v-btn>
+    </v-container>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      id: {
+        type: String,
+        required: true
+      }
+    }
+  }
+  </script>
--- src/components/ToDoList.vue
+++ src/components/ToDoList.vue
@@ -1,56 +1,65 @@
 <template>
-    <v-list>
-      <v-list-item
-        v-for="(todo, index) in todos"
-        :key="index"
-        :class="{ 'completed': todo.completed }"
-      >
-        <v-row align="center" no-gutters>
-          <v-col cols="auto" class="mr-3">
-            <v-checkbox
-              v-model="todo.completed"
-              @change="saveTodos"
-              hide-details
-            />
-          </v-col>
-          <v-col>
-            <v-list-item-content>
-              <v-list-item-title>{{ todo.text }}</v-list-item-title>
-            </v-list-item-content>
-          </v-col>
-          <v-col cols="auto">
-            <v-btn icon small @click="removeTodo(index)">
-              <v-icon>mdi-delete</v-icon>
-            </v-btn>
-          </v-col>
-        </v-row>
-      </v-list-item>
-    </v-list>
-  </template>
-  
-  <script>
-  export default {
-    name: 'TodoList',
-    props: {
-      todos: {
-        type: Array,
-        required: true
-      }
-    },
-    methods: {
-      removeTodo(index) {
-        this.$emit('remove-todo', index);
-      },
-      saveTodos() {
-        this.$emit('save-todos');
-      }
+  <v-container>
+    <v-row>
+      <v-col>
+        <h1>Vue.js To-Do App</h1>
+        <AddTodo @add-todo="addTodo" />
+        <v-list>
+          <v-list-item
+            v-for="(todo, index) in todos"
+            :key="index"
+            @click="$router.push({ name: 'ToDoDetail', params: { id: index } })"
+          >
+            <template v-slot:prepend>
+              <v-checkbox
+                v-model="todo.completed"
+                @change="saveTodos"
+                hide-details
+              />
+            </template>
+            
+            <v-list-item-title>{{ todo.text }}</v-list-item-title>
+            
+            <template v-slot:append>
+              <v-btn icon="mdi-delete" size="small" @click.stop="removeTodo(index)">
+              </v-btn>
+            </template>
+          </v-list-item>
+        </v-list>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import AddTodo from './AddTodo.vue'
+
+export default {
+  name: 'ToDoList',
+  components: { AddTodo },
+  data() {
+    return {
+      todos: []
     }
-  };
-  </script>
-  
-  <style scoped>
-  .completed .v-list-item__title {
-    text-decoration: line-through;
-    color: grey;
+  },
+  mounted() {
+    const savedTodos = localStorage.getItem('todos')
+    if (savedTodos) {
+      this.todos = JSON.parse(savedTodos)
+    }
+  },
+  methods: {
+    addTodo(newTodo) {
+      this.todos.push({ text: newTodo, completed: false })
+      this.saveTodos()
+    },
+    removeTodo(index) {
+      this.todos.splice(index, 1)
+      this.saveTodos()
+    },
+    saveTodos() {
+      localStorage.setItem('todos', JSON.stringify(this.todos))
+    }
   }
-  </style>
+}
+</script>
